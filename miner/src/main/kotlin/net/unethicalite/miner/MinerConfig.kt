fde4a9b8e5dba5d7b4d9a212bf73843c9a5d273c
DeadlyGoods <100453679+DeadlyGoods@users.noreply.github.com>
2023-05-15 16:21:40 -0400

    Update MinerConfig.kt

--- miner/src/main/kotlin/net/unethicalite/miner/MinerConfig.kt
+++ miner/src/main/kotlin/net/unethicalite/miner/MinerConfig.kt
@@ -86,7 +86,7 @@ interface MinerConfig : Config {
     )
     @JvmDefault
     fun rockType(): Rock {
-        return Rock.IRON,Rock.TIN,Rock.COOPER;
+        return Rock.TIN;
     }
     @ConfigItem(
         keyName = "radius",
