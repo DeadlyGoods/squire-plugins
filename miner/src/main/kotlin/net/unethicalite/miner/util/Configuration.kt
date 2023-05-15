ab0b71dafa9ab92b557367ed2b27e54990c48861
DeadlyGoods <100453679+DeadlyGoods@users.noreply.github.com>
2023-05-15 16:09:59 -0400

    Update Configuration.kt

--- miner/src/main/kotlin/net/unethicalite/miner/util/Configuration.kt
+++ miner/src/main/kotlin/net/unethicalite/miner/util/Configuration.kt
@@ -4,5 +4,8 @@ import net.runelite.api.ItemID
 
 
 enum class Rock(val item: Int, var rockId: IntArray) {
-    IRON(ItemID.IRON_ORE, intArrayOf(11364, 11365));
+    IRON(ItemID.IRON_ORE, intArrayOf(11364, 11365)),
+     TIN(ItemID.TIN_ORE, intArrayOf(11360, 11361)),
+      COPPER(ItemID.COPPER_ORE, intArrayOf(10943, 11161));
+    
 }
